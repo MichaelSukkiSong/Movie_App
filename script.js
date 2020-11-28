c93906a88cd214963925e328963ed53dd310f641
sa77irak <sa77irak@gmail.com>
2020-11-28 16:39:37 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/* **************************
+6. Movie App
+- https://uidesigndaily.com/posts/photoshop-movie-app-mobile-day-193
+- list of movies
+- movie info
+- fav movie
+- actors
+************************** */
